e5fea563cae0f166b045fc3ef5802e518f525b51
roaxofl1 <taeri1031@naver.com>
2026-08-19 16:54:59 +0900

    Add files via upload

--- 04_click/js/script.js
+++ 04_click/js/script.js
@@ -1,50 +1,66 @@
-// fadeOut 버튼
+// fadeOut
 $(".fadeOutBtn").click(function() {
+
     $(".box1").fadeOut();
+
 });
 
 
-// fadeIn 버튼
+// fadeIn
 $(".fadeInBtn").click(function() {
+
     $(".box1").fadeIn();
+
 });
 
 
-// fadeToggle 버튼
+// fadeToggle
 $(".fadeToggleBtn").click(function() {
+
     $(".box2").fadeToggle();
+
 });
 
 
-// slideUp 버튼
+// slideUp
 $(".slideUpBtn").click(function() {
+
     $(".box4").slideUp();
+
 });
 
 
-// slideDown 버튼
+// slideDown
 $(".slideDownBtn").click(function() {
+
     $(".box4").slideDown();
+
 });
 
 
-// slideToggle 버튼
+// slideToggle
 $(".slideToggleBtn").click(function() {
+
     $(".box5").slideToggle();
+
 });
 
 
-// ani1 버튼
+// ani1
 $(".ani1Btn").click(function() {
+
     $(".box6").animate({
         left: "500px"
     }, 1000);
+
 });
 
 
-// ani2 버튼
+// ani2
 $(".ani2Btn").click(function() {
+
     $(".box6").animate({
         left: "0px"
     }, 1000);
+
 });
